f4f4bc5fe886446ce818d85583e271dbcf5f82ec
krishmal2004 <krishmaldinidu5466@gmail.com>
2026-01-16 00:33:06 +0530

    fire base backend

---
+++ lib/auth_service.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<User?> signIn(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return result.user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Stream<QuerySnapshot> getTasks(String title) {
+    return _firestore.collection('tasks').snapshots();
+  }
+
+  Future<void> addTask(String title) async {
+    await _firestore.collection('tasks').add({
+      'title': title,
+      'isDone': false,
+      'createdAt': FieldValue.serverTimestamp(),
+    });
+  }
+}
